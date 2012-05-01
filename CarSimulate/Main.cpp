7871e03f360bec0e9b94f3b22972f8e32143e384
Mohammed Aljobory <mhdaljobory@gmail.com>
2012-05-01 17:30:45 +0200

    Deleted Unwanted Lines

--- CarSimulate/Main.cpp
+++ CarSimulate/Main.cpp
@@ -1,22 +1,9 @@
-/*
- *		This Code Was Created By Ben Humphrey 2001
- *		If You've Found This Code Useful, Please Let Me Know.
- *		Visit NeHe Productions At http://nehe.gamedev.net
- */
-
 #include <windows.h>									// Header File For Windows
 #include <stdio.h>										// Header file for standard input output (IE, "FILE") (NEW)
 #include <gl\gl.h>										// Header File For The OpenGL32 Library
 #include <gl\glu.h>										// Header File For The GLu32 Library
 #include <gl\glaux.h>									// Header File For The Glaux Library
 #include "Terrain.h"
-
-#pragma comment(lib, "opengl32.lib")					// Link OpenGL32.lib
-#pragma comment(lib, "glu32.lib")						// Link Glu32.lib
-
-//#define		MAP_SIZE	  1024							// Size Of Our .RAW Height Map (NEW)
-//#define		STEP_SIZE	  16							// Width And Height Of Each Quad (NEW)
-//#define		HEIGHT_RATIO  1.5f							// Ratio That The Y Is Scaled According To The X And Z (NEW)
 
 HDC			hDC=NULL;									// Private GDI Device Context
 HGLRC		hRC=NULL;									// Permanent Rendering Context
@@ -26,11 +13,6 @@ HINSTANCE	hInstance;									// Holds The Instance Of The Application
 bool		keys[256];									// Array Used For The Keyboard Routine
 bool		active=TRUE;								// Window Active Flag Set To TRUE By Default
 bool		fullscreen=TRUE;							// Fullscreen Flag Set To TRUE By Default
-//bool		bRender = TRUE;								// Polygon Flag Set To TRUE By Default (NEW)
-
-//BYTE g_HeightMap[MAP_SIZE*MAP_SIZE];					// Holds The Height Map Data (NEW)
-
-//float scaleValue = 0.15f;								// Scale Value For The Terrain (NEW)
 
 Terrain* terrain;
 
@@ -53,37 +35,6 @@ GLvoid ReSizeGLScene(GLsizei width, GLsizei height)		// Resize And Initialize Th
 	glLoadIdentity();									// Reset The Modelview Matrix
 }
 
-// Loads The .RAW File And Stores It In pHeightMap
-void LoadRawFile(LPSTR strName, int nSize, BYTE *pHeightMap)
-{
-	FILE *pFile = NULL;
-
-	// Open The File In Read / Binary Mode.
-	pFile = fopen( strName, "rb" );
-
-	// Check To See If We Found The File And Could Open It
-	if ( pFile == NULL )	
-	{
-		// Display Error Message And Stop The Function
-		MessageBox(NULL, "Can't Find The Height Map!", "Error", MB_OK);
-		return;
-	}
-
-	fread( pHeightMap, 1, nSize, pFile );
-
-	// After We Read The Data, It's A Good Idea To Check If Everything Read Fine
-	int result = ferror( pFile );
-
-	// Check If We Received An Error
-	if (result)
-	{
-		MessageBox(NULL, "Failed To Get Data!", "Error", MB_OK);
-	}
-
-	// Close The File.
-	fclose(pFile);
-}
-
 int InitGL(GLvoid)										// All Setup For OpenGL Goes Here
 {
 	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
@@ -93,101 +44,16 @@ int InitGL(GLvoid)										// All Setup For OpenGL Goes Here
 	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
 
-	//LoadRawFile("Data/Terrain.raw", MAP_SIZE * MAP_SIZE, g_HeightMap);	// (NEW)
-
 	terrain = new Terrain(keys);
 	terrain->LoadRawFile("Data/Terrain.raw");
 
 	return TRUE;										// Initialization Went OK
 }
 
-/*
-int Height(BYTE *pHeightMap, int X, int Y)				// This Returns The Height From A Height Map Index
-{
-	int x = X % MAP_SIZE;								// Error Check Our x Value
-	int y = Y % MAP_SIZE;								// Error Check Our y Value
-
-	if(!pHeightMap) return 0;							// Make Sure Our Data Is Valid
-
-	return pHeightMap[x + (y * MAP_SIZE)];				// Index Into Our Height Array And Return The Height
-}
-														
-void SetVertexColor(BYTE *pHeightMap, int x, int y)		// Sets The Color Value For A Particular Index, Depending On The Height Index
-{
-	if(!pHeightMap) return;								// Make Sure Our Height Data Is Valid
-
-	float fColor = -0.15f + (Height(pHeightMap, x, y ) / 256.0f);
-
-	// Assign This Blue Shade To The Current Vertex
-	glColor3f(0, 0, fColor );
-}
-
-void RenderHeightMap(BYTE pHeightMap[])					// This Renders The Height Map As Quads
-{
-	int X = 0, Y = 0;									// Create Some Variables To Walk The Array With.
-	int x, y, z;										// Create Some Variables For Readability
-
-	if(!pHeightMap) return;								// Make Sure Our Height Data Is Valid
-
-	if(bRender)											// What We Want To Render
-		glBegin( GL_QUADS );							// Render Polygons
-	else 
-		glBegin( GL_LINES );							// Render Lines Instead
-
-	for ( X = 0; X < (MAP_SIZE-STEP_SIZE); X += STEP_SIZE )
-		for ( Y = 0; Y < (MAP_SIZE-STEP_SIZE); Y += STEP_SIZE )
-		{
-			// Get The (X, Y, Z) Value For The Bottom Left Vertex
-			x = X;							
-			y = Height(pHeightMap, X, Y );	
-			z = Y;							
-
-			// Set The Color Value Of The Current Vertex
-			SetVertexColor(pHeightMap, x, z);
-
-			glVertex3i(x, y, z);						// Send This Vertex To OpenGL To Be Rendered (Integer Points Are Faster)
-
-			// Get The (X, Y, Z) Value For The Top Left Vertex
-			x = X;										
-			y = Height(pHeightMap, X, Y + STEP_SIZE );  
-			z = Y + STEP_SIZE ;							
-			
-			// Set The Color Value Of The Current Vertex
-			SetVertexColor(pHeightMap, x, z);
-
-			glVertex3i(x, y, z);						// Send This Vertex To OpenGL To Be Rendered
-
-			// Get The (X, Y, Z) Value For The Top Right Vertex
-			x = X + STEP_SIZE; 
-			y = Height(pHeightMap, X + STEP_SIZE, Y + STEP_SIZE ); 
-			z = Y + STEP_SIZE ;
-
-			// Set The Color Value Of The Current Vertex
-			SetVertexColor(pHeightMap, x, z);
-			
-			glVertex3i(x, y, z);						// Send This Vertex To OpenGL To Be Rendered
-
-			// Get The (X, Y, Z) Value For The Bottom Right Vertex
-			x = X + STEP_SIZE; 
-			y = Height(pHeightMap, X + STEP_SIZE, Y ); 
-			z = Y;
-
-			// Set The Color Value Of The Current Vertex
-			SetVertexColor(pHeightMap, x, z);
-
-			glVertex3i(x, y, z);						// Send This Vertex To OpenGL To Be Rendered
-		}
-	glEnd();
-
-	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);					// Reset The Color
-}
-*/
 int DrawGLScene(GLvoid)									// Here's Where We Do All The Drawing
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
 	glLoadIdentity();									// Reset The Matrix
-	
-	// 			 Position	      View		Up Vector
 	gluLookAt(212, 60, 194,  186, 55, 171,  0, 1, 0);	// This Determines Where The Camera's Position And View Is
 
 	terrain->Draw();
