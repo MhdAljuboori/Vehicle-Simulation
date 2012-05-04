3f82ba2906ecd6b1ad7149425862d55a808a2075
Mohammed Aljobory <mhdaljobory@gmail.com>
2012-05-04 20:13:32 +0200

    Finished Terrain

--- CarSimulate/Terrain.cpp
+++ CarSimulate/Terrain.cpp
@@ -1,47 +1,47 @@
 #include "Terrain.h"
 
-Terrain::Terrain(bool keys, int posX, int posY, int posZ,
-	int mapSize, int stepSize, float heightRatio)
+Terrain::Terrain(bool keys, //int posX, int posY, int posZ,
+	int mapSize, float stepSize)//, float heightRatio)
 {
-	if (!LoadGLTextures()) // Jump To Texture Loading Routine ( NEW )
-		return; // If Texture Didn't Load Return FALSE ( NEW )
+	//if (!LoadGLTextures()) // Jump To Texture Loading Routine ( NEW )
+	//	return; // If Texture Didn't Load Return FALSE ( NEW )
 
 	this->mapSize = mapSize;
 	this->stepSize = stepSize;
-	this->heightRatio = heightRatio;
-	this->posX = posX;
-	this->posY = posY;
-	this->posZ = posZ;
-
-	bRender = TRUE;
-	scaleValue = 0.15f;	
-	g_HeightMap = new BYTE[mapSize * mapSize];
+	//this->heightRatio = heightRatio;
+	//this->posX = posX;
+	//this->posY = posY;
+	//this->posZ = posZ;
+	terraintexture = -1;
+	WireRender = FALSE;
+	scaleValue = 0.6f;	
+	HeightMap = new BYTE[mapSize * mapSize];
 }
 
 int Terrain::getMapSize() 
 { return mapSize; }
 
-int Terrain::getStepSize()
+float Terrain::getStepSize()
 { return stepSize; }
-
+/*
 float Terrain::getHeightRatio()
 { return heightRatio; }
-
-BYTE* Terrain::getG_HeightMap() 
-{ return g_HeightMap; }
+*/
+GLubyte* Terrain::getG_HeightMap() 
+{ return HeightMap; }
 
 void Terrain::setWire()
-{ bRender = FALSE; }
+{ WireRender = FALSE; }
 
 void Terrain::setPolygon()
-{ bRender = TRUE; }
+{ WireRender = TRUE; }
 
 void Terrain::setWireNot()
-{ bRender != bRender; }
+{ WireRender != WireRender; }
 
 bool Terrain::IsPolygon()
-{ return bRender; }
-
+{ return WireRender; }
+/*
 void Terrain::LoadRawFile(LPSTR strName)
 {
 	int nSize = mapSize * mapSize;
@@ -154,7 +154,7 @@ void Terrain::SetVertexColor(int x, int y)
 		glTexCoord2f(1, 0);
 	if (i == 1000)
 		glTexCoord2f(1, 1);
-	i++;*/
+	i++;*//*
 	glColor3f(0, fColor, 0);
 }
 
@@ -229,13 +229,125 @@ void Terrain::RenderHeightMap()
 	glEnd();
 	glColor4f(1.0f, 1.0f, 1.0f, 1.0f); // Reset The Color
 }
+*/
+void Terrain::LoadHeightMap(char *filename)
+{
+    int i, j=0;
+    FILE *textureFile;
+
+    BITMAPFILEHEADER fileheader;
+    BITMAPINFOHEADER infoheader;
+
+	// Check To See If We Found The File And Could Open It
+	if( (textureFile = fopen(filename, "rb"))==NULL) 
+	{
+		// Display Error Message And Stop The Function
+		MessageBox(NULL, "Can't Find The Height Map!", "Error", MB_OK);
+		return;
+	}
+
+	// Here We Load The File Into Our HeightMap Data Array
+	fread(&fileheader, sizeof(fileheader), 1, textureFile);
+
+    fseek(textureFile, sizeof(fileheader), SEEK_SET);
+
+    fread(&infoheader, sizeof(infoheader), 1, textureFile);
+
+	HeightMap = new GLubyte[infoheader.biWidth * infoheader.biHeight];
+
+	fread(HeightMap, sizeof(GLubyte), infoheader.biWidth*infoheader.biHeight, textureFile);
+
+    fclose(textureFile);
+}
+
+void Terrain::LoadTexture(char *filename,int alpha) 
+{
+	//int num_texture = -1;
+    int i, j=0; //Index variables
+    FILE *l_file; //File pointer
+    unsigned char *l_texture; //The pointer to the memory zone in which we will load the texture
+     
+    // windows.h gives us these types to work with the Bitmap files
+    BITMAPFILEHEADER fileheader; 
+    BITMAPINFOHEADER infoheader;
+    RGBTRIPLE rgb;
+
+	terraintexture++; // The counter of the current texture is increased
+	
+	// Open the file for reading
+    if( (l_file = fopen(filename, "rb"))==NULL) 
+	{
+		terraintexture = -1;
+	}
+    
+    fread(&fileheader, sizeof(fileheader), 1, l_file); // Read the fileheader
+    
+    fseek(l_file, sizeof(fileheader), SEEK_SET); // Jump the fileheader
+    fread(&infoheader, sizeof(infoheader), 1, l_file); // and read the infoheader
+
+    // Now we need to allocate the memory for our image (width * height * color deep)
+    l_texture = (byte *) malloc(infoheader.biWidth * infoheader.biHeight * 4);
+    // And fill it with zeros
+    memset(l_texture, 0, infoheader.biWidth * infoheader.biHeight * 4);
+ 
+    // At this point we can read every pixel of the image
+    for (i=0; i < infoheader.biWidth*infoheader.biHeight; i++)
+    {            
+            // We load an RGB value from the file
+            fread(&rgb, sizeof(rgb), 1, l_file); 
+
+            // And store it
+            l_texture[j+0] = rgb.rgbtRed; // Red component
+            l_texture[j+1] = rgb.rgbtGreen; // Green component
+            l_texture[j+2] = rgb.rgbtBlue; // Blue component
+            l_texture[j+3] = alpha; // Alpha value
+            j += 4; // Go to the next position
+    }
+
+    fclose(l_file); // Closes the file stream
+     
+	glBindTexture(GL_TEXTURE_2D, terraintexture); // Bind the ID texture specified by the 2nd parameter
+
+    // The next commands sets the texture parameters
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // If the u,v coordinates overflow the range 0,1 the image is repeated
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // The magnification function ("linear" produces better results)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //The minifying function
+
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECR );
+
+    // Finally we define the 2d texture
+    glTexImage2D(GL_TEXTURE_2D, 0, 4, infoheader.biWidth, infoheader.biHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, l_texture);
+
+    // And create 2d mipmaps for the minifying function
+    gluBuild2DMipmaps(GL_TEXTURE_2D, 4, infoheader.biWidth, infoheader.biHeight, GL_RGBA, GL_UNSIGNED_BYTE, l_texture);
+
+    free(l_texture); // Free the memory we used to load the texture
+
+    //terraintexture = num_texture; // Returns the current texture OpenGL ID
+}
 
 void Terrain::Draw()
 {
 	glPushMatrix();
-	glScalef(scaleValue, scaleValue * heightRatio, scaleValue);
-	glBindTexture(GL_TEXTURE_2D, texture[0]); // Select Our Texture
-	RenderHeightMap();
+	glScaled(2, 2, 2);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D,terraintexture);
+	glScalef(1,scaleValue,1);
+	glTranslatef(-mapSize/2.0 ,0, -mapSize/2.0);
+	for (int i = 3 ; i<mapSize-3 ; i++)
+	{
+		glBegin(GL_QUAD_STRIP);
+		for (int j=3 ; j<=mapSize-3 ; j++)
+		{
+			glTexCoord2f(j*stepSize,i*stepSize);
+			glVertex3f(j,GetValue(j,i),i);
+			
+			glTexCoord2f(j*stepSize,(i+1)*stepSize);
+			glVertex3f(j,GetValue(j,i+1),i+1);
+		}
+		glEnd();
+	}
 	glPopMatrix();
 }
 
--- CarSimulate/Terrain.h
+++ CarSimulate/Terrain.h
@@ -1,33 +1,33 @@
 #pragma once
 
-#include <windows.h> // Header File For Windows
-#include <stdio.h> // Header file For Standard Input/Output
-#include <gl\gl.h> // Header File For The OpenGL32 Library
-#include <gl\glu.h> // Header File For The GLu32 Library
-#include <gl\glaux.h> // Header File For The Glaux Library
+#include <stdio.h>
+#include <windows.h>
+#include <gl\gl.h>
+#include <gl\glu.h>
+#include <iostream>
 
 class Terrain
 {
 public:
-	Terrain(bool keys, int posX = 0, int posY = 0, int posZ = 0,
-			int mapSize = 1024, int stepSize = 16, float heightRatio = 1.5f);
+	Terrain(bool keys, //int posX = 0, int posY = 0, int posZ = 0,
+			int mapSize = 512, float stepSize = 1/512.0);//, float heightRatio = 1.5f);
 	~Terrain(void);
 
 	//get methods
 	int getMapSize();
-	int getStepSize();
-	float getHeightRatio();
+	float getStepSize();
+	//float getHeightRatio();
 	BYTE* getG_HeightMap();
 
 	// Loads The .RAW File And Stores It In g_HeightMap
-	void LoadRawFile(LPSTR strName);
+	//void LoadRawFile(LPSTR strName);
 
 	// This Returns The Height From A Height Map Index
-	int Height(int X, int Y);
+	//int Height(int X, int Y);
 	// This Sets The Color Value For A Particular Index
-	void SetVertexColor(int x, int y);
+	//void SetVertexColor(int x, int y);
 	// This Renders The Height Map As Quads
-	void RenderHeightMap();
+	//void RenderHeightMap();
 
 	void Draw();
 
@@ -40,26 +40,30 @@ public:
 	bool IsPolygon();
 	
 	// Loads A Bitmap Image
-	AUX_RGBImageRec* LoadBMP(char *Filename);
+	//AUX_RGBImageRec* LoadBMP(char *Filename);
 	// Load Bitmaps And Convert To Textures
-	int LoadGLTextures();
+	//int LoadGLTextures();
+
+	void LoadTexture(char *filename,int alpha=255);
+	void LoadHeightMap(char *filename);
+
+	inline GLubyte GetValue(int i ,int j  )  
+		{return (int)(HeightMap[i*mapSize + j]);}
 private:
 	//int i;
-	int posX, posY, posZ;
+	//int posX, posY, posZ;
+	int terraintexture;
+	
 
-	GLuint texture[1];		// Storage For One Texture
+	//GLuint texture[1];		// Storage For One Texture
 
 	int mapSize;			// Size Of Our .RAW Height Map
-	int stepSize;			// Width And Height Of Each Quad 
+	float stepSize;			// Width And Height Of Each Quad 
 							// ( low value more smooth if 1 it's mean vertex every pixel)
-	float heightRatio;		// Ratio That The Y Is Scaled According To The X And Z
+	//float heightRatio;		// Ratio That The Y Is Scaled According To The X And Z
 	
 
 	bool keys[256]; // Array Used For The Keyboard Routine
-	bool bRender; // Polygon Flag Set To TRUE By Default
-	BYTE* g_HeightMap; // Holds The Height Map Data
-
-
-	LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); // Declaration For WndProc
+	bool WireRender; // Polygon Flag Set To TRUE By Default
+	GLubyte* HeightMap; // Holds The Height Map Data
 };
-
